dcee7b5d8db9f9e407f0240dd33d9c209060cd85
charbel30 <charbeltannous30@gmail.com>
2023-01-09 22:51:29 -0500

    backspace and stuff added

--- app.js
+++ app.js
@@ -20,6 +20,55 @@ function toggleDarkMode() {
   }
 
 //i want to add dark mode when i hover the button and when i unhover it goes back to normal  if it contains active class  
-
-
   document.querySelector('.dark-mode-toggle').addEventListener('click', toggleDarkMode);
+  
+ ///////////////////////////
+ 
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
+
+function handleBackspace(event, input) {
+  if (event.key === "Backspace" && input.value === "") {
+    event.preventDefault();
+    const previousDiv = input.parentNode.previousElementSibling;
+    if (previousDiv) {
+      const previousInput = previousDiv.querySelector("input");
+      previousInput.focus();
+      previousInput.value = "";
+    }
+  }
+}
+
+function handleLetter(event, input) {
+  event.preventDefault();
+  input.value = event.key;
+  const nextDiv = input.parentNode.nextElementSibling;
+  if (nextDiv) {
+    const nextInput = nextDiv.querySelector("input");
+    nextInput.focus();
+  }
+}
+
+function handleOther(event) {
+  // Prevent the input of any non-letter characters
+  if (event.key.length === 1) {
+    event.preventDefault();
+  }
+}
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach(input => {
+  input.addEventListener("keydown", event => {
+    if (isLetter(event.key)) {
+      handleLetter(event, input);
+    } else if (event.key === "Backspace") {
+      handleBackspace(event, input);
+    } else {
+      handleOther(event);
+    }
+  });
+});
+
+
